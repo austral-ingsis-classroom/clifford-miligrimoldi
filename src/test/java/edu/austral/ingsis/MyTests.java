6f87267e490f3e0d61d3aa883bf7631f0d89e988
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-04-19 19:14:03 -0300

    tests

--- src/test/java/edu/austral/ingsis/MyTests.java
+++ src/test/java/edu/austral/ingsis/MyTests.java
@@ -14,88 +14,96 @@ public class MyTests {
 
   @Test
   void testTouchAlreadyExists() throws Exception {
+    // GIVEN
     Session session = new Session(new Directory("root", List.of(new File("file1.txt"))), List.of());
     Command touch = new Touch("file1.txt");
-
+    // WHEN
     Result result = touch.execute(session);
-
+    // THEN
     assertEquals("File or directory 'file1.txt' already exists", result.output());
   }
 
   @Test
   void testRmDoesNotExist() throws Exception {
+    // GIVEN
     Session session = new Session(new Directory("root", List.of()), List.of());
     Command rm = new Rm("missing.txt", false);
-
+    // WHEN
     Result result = rm.execute(session);
-
+    // THEN
     assertEquals("File or directory 'missing.txt' does not exist", result.output());
   }
 
   @Test
   void testPwdAtRoot() throws Exception {
+    // GIVEN
     Session session = new Session(new Directory("root", List.of()), List.of());
     Command pwd = new Pwd();
-
+    // WHEN
     Result result = pwd.execute(session);
-
+    // THEN
     assertEquals("/", result.output());
   }
 
   @Test
   void testMkdirAlreadyExists() throws Exception {
+    // GIVEN
     Session session =
         new Session(new Directory("root", List.of(new Directory("docs", List.of()))), List.of());
     Command mkdir = new Mkdir("docs");
-
+    // WHEN
     Result result = mkdir.execute(session);
-
+    // THEN
     assertEquals("name 'docs' already exists", result.output());
   }
 
   @Test
   void testCdNotADirectory() throws Exception {
-    Directory root = new Directory("root", List.of(new File("notadir")));
+    // GIVEN
+    Directory root = new Directory("root", List.of(new File("notDirec")));
     Session session = new Session(root, List.of());
-    Command cd = new Cd("notadir");
-
+    Command cd = new Cd("notDirec");
+    // WHEN
     Result result = cd.execute(session);
-
-    assertEquals("'notadir' is not a directory", result.output());
+    // THEN
+    assertEquals("'notDirec' is not a directory", result.output());
   }
 
   @Test
   void testCdDot() throws Exception {
+    // GIVEN
     Session session =
         new Session(
             new Directory("root", List.of(new Directory("mydir", List.of()))), List.of("mydir"));
 
     Command cd = new Cd(".");
-
+    // WHEN
     Result result = cd.execute(session);
-
+    // THEN
     assertEquals("moved to directory 'mydir'", result.output());
   }
 
   @Test
   void testCdDoubleDot() throws Exception {
+    // GIVEN
     Session session =
         new Session(
             new Directory("root", List.of(new Directory("child", List.of()))), List.of("child"));
     Command cd = new Cd("..");
-
+    // WHEN
     Result result = cd.execute(session);
-
+    // THEN
     assertEquals("moved to directory '/'", result.output());
   }
 
   @Test
   void testTouchInvalidName() throws Exception {
+    // GIVEN
     Session session = new Session(new Directory("root", List.of()), List.of());
     Command touch = new Touch("fi le.txt");
-
+    // WHEN
     Result result = touch.execute(session);
-
+    // THEN
     assertEquals("Invalid file name: 'fi le.txt'", result.output());
   }
 }
