ea6c5ac4451d3c7af64aa5200c414c0e85fd5ef2
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-04-25 18:15:36 -0300

    Removed sealing from Command interface

--- src/main/java/edu/austral/ingsis/clifford/commands/Command.java
+++ src/main/java/edu/austral/ingsis/clifford/commands/Command.java
@@ -3,6 +3,6 @@ package edu.austral.ingsis.clifford.commands;
 import edu.austral.ingsis.clifford.Result;
 import edu.austral.ingsis.clifford.Session;
 
-public sealed interface Command permits Mkdir, Cd, Touch, Ls, Rm, Pwd {
+public interface Command {
   Result execute(Session session) throws Exception;
 }
